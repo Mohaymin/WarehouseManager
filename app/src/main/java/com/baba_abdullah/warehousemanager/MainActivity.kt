d1a089962466f8830ef9d9b36d84c8c8386ba016
Mohaymin <cabdsrijon@gmail.com>
2023-10-28 23:44:50 +0600

    modify theme and launcher icon

--- app/src/main/java/com/baba_abdullah/warehousemanager/MainActivity.kt
+++ app/src/main/java/com/baba_abdullah/warehousemanager/MainActivity.kt
@@ -1,11 +1,17 @@
 package com.baba_abdullah.warehousemanager
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.baba_abdullah.warehousemanager.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private val binding: ActivityMainBinding by lazy {
+        ActivityMainBinding.inflate(layoutInflater)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
     }
 }
